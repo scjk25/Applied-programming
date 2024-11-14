fe60eb2084e5e8a5b687b40fcc1af25522b04af3
scjk25 <131284882+scjk25@users.noreply.github.com>
2024-11-14 09:19:28 -0700

    Updated project 4

---
+++ sprint 4/Myproject/Program.cs
@@ -0,0 +1,111 @@
+﻿// See https://aka.ms/new-console-templ
+using System;
+using System.Collections.Generic;
+
+namespace CustomerOrderTracking
+{
+    // Class to represent an Address
+    public class Address
+    {
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string ZipCode { get; set; }
+
+        public Address(string street, string city, string state, string zipCode)
+        {
+            Street = street;
+            City = city;
+            State = state;
+            ZipCode = zipCode;
+        }
+
+        public override string ToString()
+        {
+            return $"{Street}, {City}, {State} {ZipCode}";
+        }
+    }
+
+    // Class to represent an Order
+    public class Order
+    {
+        public int OrderId { get; set; }
+        public DateTime OrderDate { get; set; }
+        public string Product { get; set; }
+        public decimal Price { get; set; }
+
+        public Order(int orderId, DateTime orderDate, string product, decimal price)
+        {
+            OrderId = orderId;
+            OrderDate = orderDate;
+            Product = product;
+            Price = price;
+        }
+
+        public override string ToString()
+        {
+            return $"Order ID: {OrderId}, Date: {OrderDate.ToShortDateString()}, Product: {Product}, Price: ${Price}";
+        }
+    }
+
+    // Class to represent a Customer
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+        public string Name { get; set; }
+        public Address Address { get; set; }
+        public List<Order> Orders { get; set; }
+
+        public Customer(int customerId, string name, Address address)
+        {
+            CustomerId = customerId;
+            Name = name;
+            Address = address;
+            Orders = new List<Order>();
+        }
+
+        public void AddOrder(Order order)
+        {
+            Orders.Add(order);
+        }
+
+        public void DisplayCustomerInfo()
+        {
+            Console.WriteLine($"Customer ID: {CustomerId}");
+            Console.WriteLine($"Name: {Name}");
+            Console.WriteLine($"Address: {Address}");
+            Console.WriteLine("Orders:");
+            foreach (var order in Orders)
+            {
+                Console.WriteLine($" - {order}");
+            }
+            Console.WriteLine();
+        }
+    }
+
+    // Main Program
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creating some addresses
+            Address address1 = new Address("123 Maple St", "Springfield", "IL", "62704");
+            Address address2 = new Address("456 Oak St", "Columbus", "OH", "43215");
+
+            // Creating customers
+            Customer customer1 = new Customer(1, "John Doe", address1);
+            Customer customer2 = new Customer(2, "Jane Smith", address2);
+
+            // Creating orders and adding to customers
+            Order order1 = new Order(101, DateTime.Now, "Laptop", 1200.99m);
+            Order order2 = new Order(102, DateTime.Now, "Smartphone", 799.49m);
+
+            customer1.AddOrder(order1);
+            customer1.AddOrder(order2);
+
+            // Displaying information
+            customer1.DisplayCustomerInfo();
+            customer2.DisplayCustomerInfo();
+        }
+    }
+}
---
+++ sprint 4/Myproject/Readme.md
@@ -0,0 +1,23 @@
+## Software Description
+- This software uses c# to show a customers order when they order somtthing online. It stores thier address, name, and what they ordered. 
+
+## Purpose 
+- This software is useful for anyone who wants to see what they ordered and to see if they have the correct address. This software can also be useful for the company to see what the customer ordered and to see if they have the correct address.
+
+## Tools Used
+- c#
+- dotnet framework
+- visual studio code
+- github
+
+## c# module 
+- c# is a programming language that you can use to create software. In this project we used c# to create a software that shows a customers order when they order something online. I used the dotnet framework to create the software and visual studio code to write the code.
+
+## Useful websites 
+- [Dotnet](https://dotnet.microsoft.com/en-us/download)
+- [w3 Schools](https://www.w3schools.com)
+- [github](https://github.com)
+
+
+## Youtube video
+- [Youtube] (https://youtu.be/DF2b4bfKGZs)
